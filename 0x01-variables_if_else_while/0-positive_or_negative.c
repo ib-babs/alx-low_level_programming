1605c1f69ab209c2e828f8b277c37cf29c5d3f98
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-14 10:01:14 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,14 +4,13 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	/* Find the variable n comparison */
 	int n;
 
 	srand(time(0));
