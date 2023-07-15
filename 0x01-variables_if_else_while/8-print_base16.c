30c312d6ebc5f58541ac9d3a3e08dca8e0721d6c
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-14 21:15:21 -0700

    Betty ammendment

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,2 +1,23 @@
 #include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+	char alpha = 'a';
 
+	for (n; n <= 10; n++)
+	{
+		putchar(n);
+	}
+
+	for (alpha; alpha <= 'f'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
