549827b5a79a53c965284a293f4a35e7097cca4c
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-15 06:58:57 -0700

    101-print_comb4.c file: Print triple numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a;
+	int b;
+	int i;
+
+	for (a = 0; a <= 7; a++)
+	{
+		for (b = 1; b <= 8; b++)
+		{
+			for (i = 2; i <= 9; i++)
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(i + '0');
+				if ((a != 8) || (i != 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
