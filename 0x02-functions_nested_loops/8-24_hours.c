fd25cfb93bfae576998ae5e73c483937f271156f
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-18 15:58:15 -0700

    8-24_hours.ce: prints last time in number

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry point
+ *
+ * Return: 24hours of time (Success)
+ */
+void jack_bauer(void)
+{
+	int start = 0;
+
+	while (start < 3)
+	{
+		int end = 0;
+
+		while (end < 4)
+		{
+			int sec_1 = 0;
+
+			while (sec_1 < 6)
+			{
+				int sec_2 = 0;
+
+				while (sec_2 <= 9)
+				{
+					_putchar(start + '0');
+					_putchar(end + '0');
+					_putchar(':');
+					_putchar(sec_1 + '0');
+					_putchar(sec_2 + '0');
+					_putchar('\n');
+					sec_2++;
+				}
+				sec_1++;
+			}
+			end++;
+		}
+		start++;
+	}
+	return (0);
+}
+
