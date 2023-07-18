fdddebade7410b4a1b5e8eeec6a6826149c6cdc6
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-18 14:29:02 -0700

    7-print_last_digit.c file: prints last digit of number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *
+ * @n:  Number to test
+ *
+ * Return: Always 0 (Success)
+ */
+int print_last_digit(int n)
+{
+	_putchar((n % 10) + '0');
+
+}
