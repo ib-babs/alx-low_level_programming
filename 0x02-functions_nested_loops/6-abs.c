b9b2c6283c4e677b9eca7a8cedb5863bb662f866
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-18 14:20:09 -0700

    6-abs.c file: return absolute integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _abs - Entry point
+ *
+ * @n:  Number to test
+ *
+ * Return: Absolute integer (Success)
+ */
+int _abs(int n)
+{
+	return (abs(n));
+
+}
