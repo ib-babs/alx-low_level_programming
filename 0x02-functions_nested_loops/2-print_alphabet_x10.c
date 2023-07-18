d061c00070ac45538ea58403f261f297f768fe42
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-18 13:15:16 -0700

    2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+
+	while (count <= 10)
+	{
+		char a = 'a';
+
+		while (a <= 'z')
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+		count++;
+	}
+	_putchar('\n');
+}
