502ad3756e8744220f57a7e13d343c22a4a36b02
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-19 08:28:55 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int t1, t2, i, sum;
+
+	t1 = 0;
+
+	t2 = 1;
+
+	sum = t1 + t2;
+
+	for (i = 0; i <= 10; i++)
+	{
+		t1 = t2;
+
+		t2 = sum;
+
+		sum = t1 + t2;
+		printf("%d", t2);
+		if (i != 10)
+		{
+			printf(", ");
+		}
+	}
+	return (0);
+}
