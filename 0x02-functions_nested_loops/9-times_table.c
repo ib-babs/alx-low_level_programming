4d2026e6887ea739b14033218fba1e05bcf76b99
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-19 04:07:39 -0700

    9-times_table.c file: print time table of 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * times_table -Entry point
+ *
+ * Return: multiplication table of 9
+ */
+void times_table(void)
+{
+	int m = 0;
+
+	while (m < 1)
+	{
+		int n = 0;
+
+		while (n < 1)
+		{
+			int p = 0;
+
+			while (p <= 9)
+			{
+				int q = 0;
+
+				while (q <= 9)
+				{
+					printf("%2d",  p * q);
+					if ((q < 9))
+					{
+						printf(",");
+						printf(" ");
+					}
+					q++;
+				}
+				printf("\n");
+				p++;
+			}
+			printf(" ");
+			n++;
+		}
+		m++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ int print_last_digit(int n);
 void jack_bauer(void);
 int add(int num_1, int num_2);
 void print_to_98(int n);
+void times_table(void);
