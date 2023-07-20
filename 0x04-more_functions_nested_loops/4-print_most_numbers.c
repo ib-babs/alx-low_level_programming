6781f1e7d21362390ae617cb077acac4817386a8
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-20 14:51:23 -0700

    Another commit void print_numbers(void) prototype

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * print_most_numbers - print number from 0 to 9
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 2 || n == 4)
+			continue;
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
