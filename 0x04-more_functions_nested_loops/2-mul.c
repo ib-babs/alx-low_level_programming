f469f8fba02b212439a8d73615925ab61df32671
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-20 14:30:47 -0700

    Another commit int mul(int a, int b);

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - return a multiply by b
+ * @a: first number
+ * @b: second number
+ * Return: multiplication of two numbers
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
