02341b1c546d71cc1dbc949750647b012142ef93
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-21 10:51:56 -0700

    6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * print_line - Prints line of specified number
+ *
+ * @n: Number specified
+ *
+ * Return: void
+ */
+void print_line(int n)
+{
+	int a;
+
+	if (a > n)
+	{
+	for (a = 0; a < n; a++)
+		putchar('_');
+	}
+	putchar('\n');
+}
