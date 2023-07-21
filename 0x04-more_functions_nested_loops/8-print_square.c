5d94c764ac67ce710c9b72dd69413c86e1525140
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-21 16:16:21 -0700

    void print_square function

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * print_square - print squre shape
+ * @size: size of the shape
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j, k;
+
+	char c = '#';
+
+	if (size > 0)
+	{
+		for (j = 0; j < size; j++)
+		{
+			for (i = 0; i < size; i++)
+				putchar(c);
+			putchar('\n');
+		}
+	}
+	else
+		putchar('\n');
+}
