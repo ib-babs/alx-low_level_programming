cbbec4b34feefc2793e0fc494abdd3d5d3efe46c
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-21 15:48:51 -0700

    7-print_diagonal.c file to print diagonal lines

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * print_diagonal - Print diagonal of specified number
+ * @n: Specified number of times
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int j, k;
+
+	if (n > 0)
+	{
+		for (j = 0; j < n; j++)
+		{
+			for (k = 0; k <= j; k++)
+			{
+				if (k > 0)
+					putchar(' ');
+			}
+			putchar('\\');
+		}
+	}
+	putchar('\n');
+}
