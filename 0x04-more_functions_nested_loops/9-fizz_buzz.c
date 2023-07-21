33a4f8164e3c623f28bd11907d577d940beb3d5b
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-21 16:50:19 -0700

    9-fizz_buzz.c hot fixed

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,26 +8,27 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	for (n = 1; n < 101; n++)
 	{
+		if ((n > 1) && (n != 101))
+			printf(" ");
+
 		if ((n % 3 == 0) && !(n % 5 == 0))
 		{
-			printf("Fizz ");
+			printf("Fizz");
 			continue;
 		}
 		else if ((n % 5 == 0) && !(n % 3 == 0))
 		{
 			printf("Buzz");
-			if (n != 100)
-				printf(" ");
 			continue;
 		}
 		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 			continue;
 		}
-		printf("%d ", n);
+		printf("%d", n);
 	}
 	return (0);
 }
