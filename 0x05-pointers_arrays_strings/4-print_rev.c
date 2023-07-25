e8cab93b4bcd5d86a4fe379896f072fcc5ae5999
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-25 12:35:24 -0700

    4-print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - Print reverse of input string
+ * @s: Character to input
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int len = strlen(s), i;
+
+	for (i = 0; i < len; i++)
+		printf("%c", s[len - i - 1]);
+}
