30d851187999332e2807defa219d5a9a95b4dc3d
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-25 12:55:04 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * puts2 - Return odd character
+ * @str: Pointer to the string
+ * Return - void
+ */
+void puts2(char *str)
+{
+	int len = strlen(str), i;
+
+	for (i = 0; i <= len; i++)
+	{
+		if (i % 2 == 1)
+			continue;
+		printf("%c", str[i]);
+	}
+	putchar('\n');
+}
