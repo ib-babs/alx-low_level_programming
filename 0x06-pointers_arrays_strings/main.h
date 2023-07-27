50ff467c0d033214b668d7b45062c2ae2575e88b
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-27 10:48:19 -0700

    leet it

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
 char *_strcat(char *dest, char *src);
 void reverse_array(int *a, int n);
 char *string_toupper(char *s);
-char *cap_string(char *);
+char *cap_string(char *s);
+char *leet(char *s);
