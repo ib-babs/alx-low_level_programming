23840845a0229a7b45b396822a466f9ca488e504
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-27 10:02:14 -0700

    Convert string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,10 @@
+#include <string.h>
+/**
+ * string_toupper - Change string to uppercase
+ * @s: String to convert
+ * Return: Converted string
+ */
+char *string_toupper(char *s)
+{
+	return (strupr(s));
+}
