462918211864211217f33e1bda36dca2404374c2
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-31 15:58:54 -0700

    My 3rd commit

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * _strchr - Main pointer entry
+ * @s: String pointer to test
+ * @c: Character to process
+ * Return: String location
+*/
+char *_strchr(char *s, char c)
+{
+	return (strchr(s, c));
+}
