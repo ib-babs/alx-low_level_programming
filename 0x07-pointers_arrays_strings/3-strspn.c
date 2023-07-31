1c5ba4eb5a6f726ca00936c1d1ff2fcc4eb1d461
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-31 16:01:12 -0700

    My 4th commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * _strspn - Main pointer function
+ * @s: Array of string to test
+ * @accept: String accepted
+ * Return: Manipulated string
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	return (strspn(s, accept));
+}
