846fb04e9dacb68e6363ed4b17f9068637cd364b
Ibrahim <babatundeibrahim426@gmail.com>
2023-07-31 16:12:23 -0700

    My 7th commits

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_chessboard - Main entry
+ * @a: Array of two dimensional
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			printf("%c", *(*a + j));
+		**a++;
+		printf("\n");
+	}
+}
