bb9ad009b4051825c18ae0cd937f208d8ffa8a1c
Ibrahim <babatundeibrahim426@gmail.com>
2023-08-01 09:05:36 -0700

    Initial

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,18 +7,12 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, diagsum1 = 0, diagsum2 = 0;
+	int j, diagsum1 = 0, diagsum2 = 0;
 
-	for (i = 0; i < size; i++)
+	for (j = 0; j < size; j++)
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j && !(i > j))
-				diagsum1 += a[j - i];
-			else if (j > i && !(i == j))
-				diagsum2 += a[j - i];
-			a++;
-		}
+		diagsum1 += a[(size * j) + j];
+		diagsum2 += a[size * (j + 1) - (j + 1)];
 	}
 	printf("%d, %d\n", diagsum1, diagsum2);
 }
