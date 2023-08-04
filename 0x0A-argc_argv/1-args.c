18952af1495d32a5f0665432a3ec826344ed0077
Ibrahim <babatundeibrahim426@gmail.com>
2023-08-04 16:07:31 -0700

    Second args commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,14 +5,8 @@
  * @argv: Arg vector
  * Return: 0
 */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)) char *argv[])
 {
-	int i = 0;
-
-	while (i < argc)
-	{
-		printf("%s\n", argv[i]);
-		i++;
-	}
+	printf("%d\n", argc);
 	return (0);
 }
