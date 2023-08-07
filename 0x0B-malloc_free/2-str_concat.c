57d66f9b57627f5d95c669f7f963d8fe22ff973b
ibrahim <babatundeibrahim426@gmail.com>
2023-08-07 17:40:54 +0100

    Third commit

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include "main.h"
+/**
+ * str_concat - Function pointer
+ * @s1: String 1
+ * @s2: String 2
+ * Return: Pointer to concatenated string
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *ptr;
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		s1 = "";
+		s2 = "";
+	}
+	if (ptr == NULL)
+		return (NULL);
+	ptr = strcat(s1, s2);
+	return (ptr);
+}
