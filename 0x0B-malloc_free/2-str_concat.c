a20eecbc8b3f8e16a63859a677e8bd7030bdf178
ibrahim <babatundeibrahim426@gmail.com>
2023-08-07 23:32:31 +0100

    String concatenation function

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "main.h"
 /**
  * str_concat - Function pointer
@@ -12,13 +13,12 @@ char *str_concat(char *s1, char *s2)
 
 	char *ptr;
 
-	if (s1 == NULL && s2 == NULL)
-	{
+	if (s1 == NULL)
 		s1 = "";
+	if (s2 == NULL)
 		s2 = "";
-	}
 
-	ptr = malloc(sizeof(ptr));
+	ptr = malloc(strlen(s1) + strlen(s2));
 
 	if (ptr == NULL)
 		return (NULL);
