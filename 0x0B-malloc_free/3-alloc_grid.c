6ac11140a59d9ee8ba087cd546f61fec0d917174
ibrahim <babatundeibrahim426@gmail.com>
2023-08-08 15:57:07 +0100

    New commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,7 @@ int **alloc_grid(int width, int height)
 
 	unsigned int j = 0;
 
-	int **ptr = malloc(width);
+	int **ptr = malloc(width * 2);
 
 	if (width <= 0 || height <= 0 || ptr == NULL)
 		return (NULL);
