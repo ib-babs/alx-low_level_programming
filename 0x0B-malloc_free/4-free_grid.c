e107b370f8afe10e4c451d9a6cc86e44e10b01c7
ibrahim <babatundeibrahim426@gmail.com>
2023-08-08 16:09:23 +0100

    Newer commit

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * free_grid - Free memory of height
+ * @grid: Pointer to 2D array
+ * @height: Column size
+ * Return: Void
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(height * sizeof(int));
+		free(grid[i]);
+	}
+}
