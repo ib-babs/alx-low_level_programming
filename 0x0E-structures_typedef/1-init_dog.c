47ca911f767cdfc3ac3aa71279f98b009561e5d4
ibrahim <babatundeibrahim426@gmail.com>
2023-08-14 20:54:54 +0100

    New commit

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+/**
+ * init_dog - Main Point
+ * @d: Pointer to the struct
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: The owner of the dog
+ * Return: Void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog *ptr = d;
+
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+}
