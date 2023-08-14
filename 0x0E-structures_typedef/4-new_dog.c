d739190c21b30162a3bdb158555d43aebf47b990
ibrahim <babatundeibrahim426@gmail.com>
2023-08-14 22:10:46 +0100

    New new commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - Structure pointer
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: The dog owner
+ * Return: Pointer to the new structure
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	int i, j;
+
+	dog_t *ptr = malloc(sizeof(name) * sizeof(age) * sizeof(char));
+
+	char *name_copy = malloc(strlen(name) + 1);
+	char *owner_copy = malloc(strlen(owner) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	i = 0;
+
+	while (name[i] != '\0')
+	{
+		name_copy[i] = name[i];
+		i++;
+	}
+	name_copy[i] = '\0';
+	free(name_copy);
+
+	j = 0;
+	while (owner[j] != '\0')
+	{
+		owner_copy[j] = owner[j];
+		j++;
+	}
+	owner_copy[j] = '\0';
+	free(owner_copy);
+
+	ptr->name = name;
+	ptr->age = age;
+	ptr->owner = owner;
+
+	return (ptr);
+	free(ptr);
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,4 +17,5 @@ typedef struct dog
 #define INIT_DOG
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 #endif /* INIT_DOG */
