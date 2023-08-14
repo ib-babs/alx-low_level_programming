16514987ac52541a03fa760d072eb057b7e4f5e2
ibrahim <babatundeibrahim426@gmail.com>
2023-08-14 20:49:16 +0100

    Struct header fixed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
+#ifndef DOG_STRUCT
+#define DOG_STRUCT
+/**
+ * struct dog - Structure for a dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: THe owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+#endif /* DOG_STRUCT */
