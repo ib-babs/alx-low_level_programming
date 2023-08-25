91bfab295db71b35ab0b05c5cb8cef2a36d075fe
ibrahim <babatundeibrahim426@gmail.com>
2023-08-25 01:01:21 +0100

    New

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void __attribute__((constructor)) hare(void);
+/**
+ * hare - A story of hare
+ * Return: Void
+ */
+void hare(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
