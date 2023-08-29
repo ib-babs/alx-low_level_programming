036f646adf5a9fc3c4acf91a15402776ffa80020
ibrahim <babatundeibrahim426@gmail.com>
2023-08-29 14:28:36 +0100

    New commit in file

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,4 @@
 #include "lists.h"
-size_t looped_listint_len(const listint_t *head);
 /**
  * looped_listint_len - Counts the number of unique nodes
  * in a looped listint_t linked list.
@@ -8,7 +7,7 @@ size_t looped_listint_len(const listint_t *head);
  * Return: If the list is not looped - 0.
  * Otherwise - the number of unique nodes in the list.
  */
-size_t looped_listint_len(const listint_t *head)
+size_t looped_listint_len(listint_t *head)
 {
 	const listint_t *tortoise, *hare;
 	size_t nodes = 1;
