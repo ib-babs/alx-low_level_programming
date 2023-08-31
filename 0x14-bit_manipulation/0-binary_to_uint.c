7850834ab0a41c05fc1a11533a40614569c55536
ibrahim <babatundeibrahim426@gmail.com>
2023-08-31 13:41:26 +0100

    First commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* binary_to_uint - Conversion from bin to int
+* @b: Binary in string
+* Return: Binary in decimal, 0 otherwise
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	int length = strlen(b) - 1;
+
+	if (b == NULL)
+		return (0);
+	while (length >= 0)
+	{
+		if (*b != 48 && *b != 49)
+			return (0);
+		sum += (int)pow(2, length) * (*b - '0');
+		*b = *b++;
+		length--;
+	}
+	return (sum);
+}
