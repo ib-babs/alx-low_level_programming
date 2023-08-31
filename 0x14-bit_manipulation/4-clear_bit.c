aef5310a688e4a5d8a6ae7bab4ee782e46eb43b7
ibrahim <babatundeibrahim426@gmail.com>
2023-08-31 16:04:28 +0100

    Fifth commit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * clear_bit - Clears bit
+ * @n: Pointer to n
+ * @index: Index
+ */
+int clear_bit(unsigned long int *n, unsigned long int index)
+{
+	if (index > sizeof(n) * 8)
+		return (-1);
+	*n &= ~(1 << index);
+	return (1);
+}
