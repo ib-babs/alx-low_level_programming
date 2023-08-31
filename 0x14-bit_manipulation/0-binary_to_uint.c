129630e9c25f688436b9da58a6fd024fa5861934
ibrahim <babatundeibrahim426@gmail.com>
2023-08-31 14:00:25 +0100

    First commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,5 +1,29 @@
 #include "main.h"
 /**
+* _pow - Power
+* @base: Base
+* @n: Number
+* Return: Power of number
+*/
+int _pow(int base, int n)
+{
+	int p;
+
+	if (n == 0)
+		p = 1;
+	else
+	{
+		p = base;
+		while (n > 1)
+		{
+			p = p * base;
+			n--;
+		}
+	}
+	return (p);
+}
+
+/**
 * binary_to_uint - Conversion from bin to int
 * @b: Binary in string
 * Return: Binary in decimal, 0 otherwise
@@ -15,7 +39,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (*b != 48 && *b != 49)
 			return (0);
-		sum += (int)pow(2, length) * (*b++ - '0');
+		sum += _pow(2, length) * (*b++ - '0');
 		length--;
 	}
 	return (sum);
