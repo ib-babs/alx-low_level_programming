aeb38e8d4567ba36e5cc043a558c83f151fa3930
ibrahim <babatundeibrahim426@gmail.com>
2023-08-31 15:44:13 +0100

    Second commit

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * _pow - Power
+ * @base: Base
+ * @n: Number
+ * Return: Power of number
+ */
+unsigned long int _pow2(unsigned int base, unsigned int n)
+{
+
+	unsigned long int p;
+
+	if (n == 0)
+		p = 1;
+	else
+	{
+		p = base;
+		while (n > 1)
+		{
+			/* code */
+			p = p * base;
+			n--;
+		}
+	}
+	return (p);
+}
+/**
+ * print_binary - As the name implies
+ * @n: Number to print
+ * Return: Void
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int divisor, checker;
+	char f;
+
+	f = 0;
+	divisor = _pow2(2, sizeof(unsigned long int) * 8 - 1);
+
+	while (divisor > 0)
+	{
+		/* code */
+		checker = n & divisor;
+		if (checker == divisor)
+		{
+			f = 1;
+			printf("%d", 1);
+		}
+		else if (f == 1 || divisor == 1)
+			printf("%d", 0);
+		divisor >>= 1;
+	}
+}
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,4 +11,5 @@ int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 int _pow(int base, int n);
 int get_endianness(void);
+unsigned long int _pow2(unsigned int base, unsigned int n);
 #endif /* MAIN_H */
