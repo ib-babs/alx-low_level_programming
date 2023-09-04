f90abf97d9622027944be70460040f81edcb013e
ibrahim <babatundeibrahim426@gmail.com>
2023-09-04 16:48:37 +0100

    Initial commit

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -3,19 +3,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, bytes_written;
 	ssize_t i;
-	char buffer[letters];
-	if (filename == NULL) // Checking for errors
-		 return (0);
+	char *buffer = malloc(letters * sizeof(char));
+	if (filename == NULL)
+		return (0);
 
-	fd = open(filename, O_RDONLY); // Open the file for reading
+	fd = open(filename, O_RDONLY);
 	bytes_written = read(fd, buffer, letters);
 
-	if (bytes_written == -1 || fd == -1) // Checking for errors
+	if (bytes_written == -1 || fd == -1)
 	{
 		close(fd);
 		return (0);
 	}
-	buffer[bytes_written] = '\0'; // Reading the file
+	buffer[bytes_written] = '\0';
 
 	for (i = 0; buffer[i]; i++)
 		putchar(buffer[i]);
