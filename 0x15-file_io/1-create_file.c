4a4f7682afaed039b35f329656ae92c6a2ad7d7b
ibrahim <babatundeibrahim426@gmail.com>
2023-09-04 17:32:58 +0100

    Initial commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-	if (!text_context)
+	if (!text_content)
 		text_content = "";
 
 	for (bytes_written = 0; text_content[bytes_written]; bytes_written++);
