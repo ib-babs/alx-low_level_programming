e5fbb6fe3b8974912653649fa9746e238abcd566
ibrahim <babatundeibrahim426@gmail.com>
2023-09-04 17:42:05 +0100

    Initial commit

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * create_file - Create a file
+ * @filename: Name if the file
+ * @text_content: Content of the file
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, bytes_written, mode_perm;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		for (bytes_written = 0; text_content[bytes_written]; bytes_written++)
+		;
+
+		mode_perm = write(fd, text_content, bytes_written);
+
+		if (mode_perm == -1)
+			return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
