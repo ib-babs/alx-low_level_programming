19afffbaa5b05c0a4ede515e6dc45fe9d92a4546
ibrahim <babatundeibrahim426@gmail.com>
2023-09-04 17:24:33 +0100

    Initial commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,19 +7,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, bytes_written;
-	size_t length = strlen(text_content);
-	char *buffer = malloc(length + 1);
+	int fd, bytes_written, mode_perm;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | S_IRUSR | S_IWUSR);
-	bytes_written = write(fd, buffer, length);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	if (!text_context)
+		text_content = "";
+
+	for (bytes_written = 0; text_content[byte_written]; bytes_written++);
+
+	mode_perm = write(fd, buffer, length);
 	buffer[length] = '\0';
 
-	if (fd == -1 || bytes_written == -1 || buffer == NULL)
+	if (mode_perm == -1)
 		return (-1);
+	close(fd);
 
-	free(buffer);
 	return (1);
 }
