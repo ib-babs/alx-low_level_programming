4662577e094ecdf754cabd81cb6730a51fc9404a
ib-babs <babatundeibrahim426@gmail.com>
2023-10-13 17:54:56 +0100

    Initial commit

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,43 +1,46 @@
 #include "lists.h"
+
 /**
-* insert_dnodeint_at_index - Insert node at index specified
-* @h: Head of the list
-* @idx: Index
-* @n: Data to be added
-* Return: New node added
-*/
+ * insert_dnodeint_at_index - function with three arguments
+ * @h: pointer to doubly linked list
+ * @idx: index position to insert node
+ * @n: value of new node
+ *
+ * Description: inserting new node into a doubly linked list
+ * Return: address of new node or NULL if failed
+ */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node = malloc(sizeof(dlistint_t));
-	dlistint_t *next_node = NULL, *prev_node = *h;
+	dlistint_t *new, *cursor;
 
-	if (!new_node)
+	if (h == NULL)
+		return (NULL);
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	/* loop until nth node of idx */
+	cursor = *h;
+	while (idx > 1 && cursor && cursor->next)
+	{
+		cursor = cursor->next;
+		idx--;
+	}
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	if (idx > 1 || cursor == NULL)
 		return (NULL);
 
-	new_node->n = n;
-	new_node->next = NULL;
-	new_node->prev = NULL;
+	/* assign value to new node */
+	new->n = n;
 
-	if (idx == 0)
-	{
-		next_node = *h;
-		*h = new_node;
-		(*h)->next = next_node;
-	}
-	else
-	{
-		while (idx > 1)
-		{
-			prev_node = prev_node->next;
-			idx--;
-		}
-		next_node = prev_node->next;
-		prev_node->next = new_node;
-		new_node->prev = prev_node;
-		new_node->next = next_node;
-		next_node->prev = new_node;
-	}
-	if (*h && idx == 0)
-		return (new_node);
-	return (NULL);
+	/* insert node */
+	if (cursor->next != NULL)
+		cursor->next->prev = new;
+	new->prev = cursor;
+	new->next = cursor->next;
+	cursor->next = new;
+
+	return (new);
 }
