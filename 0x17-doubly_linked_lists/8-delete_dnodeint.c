a828837a0e49175b0eea2e52d857c4b6cf5ceadf
ib-babs <babatundeibrahim426@gmail.com>
2023-10-13 17:58:26 +0100

    Initial commit

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,36 +1,39 @@
 #include "lists.h"
+
 /**
-* delete_dnodeint_at_index - as the name implied
-* @head: Head of the list
-* @index: Node Index
-* Return: 1 succes, -1 otherwise
-*/
+ * delete_dnodeint_at_index - function with two arguments
+ * @head: pointer to double linked list
+ * @index: index position to delete node
+ *
+ * Description: delete node at index position
+ * Return: 1 if succeeded or -1 if failed
+ */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *temp, *prev = *head;
+	dlistint_t *cursor = *head;
 
-	if (head == NULL || !(*head))
+	if (!head)
 		return (-1);
-
-	if (index == 0)
+	if (head)
 	{
-		temp = (*head)->next;
-		*head = NULL;
-		free(*head);
-		*head = temp;
-	}
-	else
-	{
-		while (index != 0)
+		while (index && cursor)
 		{
-			prev = prev->next;
+			cursor = cursor->next;
 			index--;
 		}
-		temp = prev->next;
-		temp->prev = prev->prev;
-		prev->prev->next = temp;
-		free(prev);
-		prev = NULL;
+		if (index)
+			return (-1);
+		if (!index && cursor)
+		{
+			if (cursor->next)
+				cursor->next->prev = cursor->prev;
+			if (cursor->prev)
+				cursor->prev->next = cursor->next;
+			else
+				*head = cursor->next;
+			free(cursor);
+			return (1);
+		}
 	}
-	return (1);
+	return (-1);
 }
