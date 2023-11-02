6bc68eedae1ee308d98c48e192947912c94cd663
ib-babs <babatundeibrahim426@gmail.com>
2023-11-02 22:01:44 +0100

    File 0, Readme, and header file are added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,7 +7,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
-
+	unsigned long int i;
 	if (!new_hash_table)
 		return (NULL);
 
@@ -16,17 +16,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	if (!new_hash_table->array)
 		return (NULL);
-	for (unsigned long int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		new_hash_table->array[i] = malloc(sizeof(char *));
 
 	return (new_hash_table);
-}
-
-int main(void)
-{
-	hash_table_t *ht = hash_table_create(20);
-	unsigned int i;
-	for (i = 0; ht->array[i]; i++)
-		printf("%p\n", (void *)ht->array[i]);
-	return 0;
 }
