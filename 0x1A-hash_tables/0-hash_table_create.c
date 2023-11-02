37c4109db7c72010acb985125bc6f7dd211ab6bd
ib-babs <babatundeibrahim426@gmail.com>
2023-11-02 22:04:50 +0100

    File 0, Readme, and header file are added

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,13 +8,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash_table = malloc(sizeof(hash_table_t));
 
-	if (!new_hash_table)
+	if (new_hash_table == NULL)
 		return (NULL);
 
-	/*new_hash_table->size = size;*/
+	new_hash_table->size = size;
 	new_hash_table->array = malloc(sizeof(new_hash_table->array) * size);
 
-	if (!new_hash_table->array)
+	if (new_hash_table->array == NULL)
 		return (NULL);
 
 	return (new_hash_table);
