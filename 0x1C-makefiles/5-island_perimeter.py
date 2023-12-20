b4c781b8e05a173644b8ae5ab8ef387087d7aa7b
ib-babs <babatundeibrahim426@gmail.com>
2023-12-20 22:25:25 +0100

    Initial commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -12,24 +12,23 @@ def island_perimeter(grid):
 
     for row in range(1, len(grid)):
         for col in range(1, len(grid[row])):
-            if:
-                if grid[row][col] == land_zone:
-                    env_list = [grid[row - 1][col], grid[row]
-                                [col + 1], grid[row + 1][col],
-                                grid[row][col - 1]]
-                    count = 0
-                    for i in env_list:
-                        if i == 0:
-                            count += 1
-                    if count == 4:
-                        return 4
-                    elif count == 3:
-                        perimeter += 3
-                    elif count == 2:
-                        perimeter += 2
-                    elif count == 1:
-                        perimeter += 1
-                    else:
-                        pass
+            if grid[row][col] == land_zone:
+                env_list = [grid[row - 1][col], grid[row]
+                            [col + 1], grid[row + 1][col],
+                            grid[row][col - 1]]
+                count = 0
+                for i in env_list:
+                    if i == 0:
+                        count += 1
+                if count == 4:
+                    return 4
+                elif count == 3:
+                    perimeter += 3
+                elif count == 2:
+                    perimeter += 2
+                elif count == 1:
+                    perimeter += 1
+                else:
+                    pass
 
     return perimeter
