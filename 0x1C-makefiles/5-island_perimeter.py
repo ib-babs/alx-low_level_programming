9736452fad86977a9c1f37623e0e2bc24515c1f9
ib-babs <babatundeibrahim426@gmail.com>
2023-12-20 22:23:18 +0100

    Initial commit

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,7 +11,9 @@ def island_perimeter(grid):
         if row == 0 or row == len(grid):
             continue
         for col in range(len(grid[row])):
-            if col > 0:
+            if col == 0 or col == len(grid[row]):
+                continue
+            else:
                 if grid[row][col] == land_zone:
                     env_list = [grid[row - 1][col], grid[row]
                                 [col + 1], grid[row + 1][col],
